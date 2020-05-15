2c4b5cc38f5d96a6bc2139fc6070f23d7b9e301b
Nimeshika1 <65106206+Nimeshika1@users.noreply.github.com>
2020-05-15 07:59:28 +0530

    Add files via upload

--- src/main/java/com/sysco/api_automation/api/common/Constant.java
+++ src/main/java/com/sysco/api_automation/api/common/Constant.java
@@ -3,7 +3,7 @@ package com.sysco.api_automation.api.common;
 public class Constant {
     Constant() {}
 
-    public static final String API_HOST = System.getProperty("api.host", "https://syscoqcenterapi.sysco.com/qcenter_admin_api/");
+    public static final String API_HOST = System.getProperty("api.host", "http://dummy.restapiexample.com/api/v1");
     public static final String BASE_PATH = System.getProperty("api.base", "");
 
 
